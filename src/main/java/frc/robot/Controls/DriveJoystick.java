923d017b8bba9b917f9c21af6bad901bef09974c
Spencer Boggs <spencer_boggs@icloud.com>
2022-03-13 22:26:41 -0800

    Drive Joystick Comments

--- src/main/java/frc/robot/Controls/DriveJoystick.java
+++ src/main/java/frc/robot/Controls/DriveJoystick.java
@@ -10,22 +10,27 @@ import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 
 public class DriveJoystick {
   
+  //Sets the drive joystick to the XboxController that was imported above
   public static XboxController joystick = RobotMap.driveJoystick;
   private static double lastMoveTime = 0;
 
   public static double getMove(){
+    //Sets the speed of the joystick equal to the axis of input 1 on the Xbox Controller
     double speed = joystick.getRawAxis(1); //Left joystick on controller, determines forward/backwards movement
     return speed;
   }
   public static double getTurn(){
+    //Sets the speed of the joystick equal to the axis of input 4 on the Xbox Controller
     double speed = joystick.getRawAxis(4); //Right joystick on controller, determines pivot amount
     return speed;
   }
   public static double axisFive(){
+    //Sets the axis double equal to input 4
     double axis = joystick.getRawAxis(5); //right joystick vertical
     return axis;
   }
   public static boolean getCameraOrient() {
+    //Checks if input 2 on the controller is pressed and held.
     return joystick.getRawButtonPressed(2); //B button, switches orientation of f/b controls
   }
   public static boolean getResetEncoder() {
@@ -38,15 +43,20 @@ public class DriveJoystick {
     return joystick.getRawButtonPressed(6);
   }
   public static boolean getContinueAutoOrient() {
+    //Checks if the movement of the robot or the turning of the robot is greater than 0.05
     if (Math.abs(getMove()) > 0.05 || Math.abs(getTurn()) > 0.05) {
       //Robot has moved
+      //Sets the variable "lastMoveTime" as a timer that starts when the robot last moved.
       lastMoveTime = Timer.getFPGATimestamp();
     }
+    //Displays a statistic on SmartDashboard called "last move time" that shows the time since the robot last moved
     SmartDashboard.putNumber("last move time", lastMoveTime);
+    //Returns the timer minus 0.75 seconds
     return Timer.getFPGATimestamp() - lastMoveTime > 0.75;
     //return joystick.getRawButton(5) || joystick.getRawButton(6);
   }
 
+  //More buttons...
   public static boolean getFront(){
     return joystick.getRawButtonPressed(2);
   }
@@ -56,6 +66,7 @@ public class DriveJoystick {
   }
 
   public static boolean getToggleLight(){
+    //Checks if input 3 is pressed. If it is pressed then the method returns true for one moment.
     return joystick.getRawButton(3);
   }
 
@@ -71,6 +82,7 @@ public class DriveJoystick {
 
   // SLOW MODE 
   public static boolean dPad(){
+    //If any of the following booleans are true then the output of this method is true.
     if(getPreciseFront() || getPreciseRight() || getPreciseBack() || getPreciseLeft()){
       return true;
     }
@@ -79,6 +91,7 @@ public class DriveJoystick {
     }
   }
 
+  //The following methods check when the POV of the joystick is 0, 90, 180 or 270
   public static boolean getPreciseFront(){
     if(joystick.getPOV() == 0){
       return true;
@@ -121,6 +134,7 @@ public class DriveJoystick {
   }
 
   public static boolean enablePIDMode() {
+    //Checks if the axis of input 3 is greater than 0.6
     return joystick.getRawAxis(3) > 0.6;
   }
 
@@ -135,6 +149,7 @@ public class DriveJoystick {
     return joystick.getRawAxis(2) > 0.7;
   }
 
+  //RUMBLE RUMBLE lol
   public static void rumble(double intensity) {
     joystick.setRumble(RumbleType.kLeftRumble, intensity);
     joystick.setRumble(RumbleType.kRightRumble, intensity);
