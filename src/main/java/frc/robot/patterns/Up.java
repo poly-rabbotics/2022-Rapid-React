e79071e879f6cbf94884bfa598d9003547256ece
JustBeChill <kmalling16@gmail.com>
2022-10-08 11:33:22 -0700

    Updated up.java

--- src/main/java/frc/robot/patterns/Up.java
+++ src/main/java/frc/robot/patterns/Up.java
@@ -1,6 +1,6 @@
 package frc.robot.patterns;
-
-import frc.robot.subsystems.helperClasses.LightPattern;
+import frc.robot.subsystems.helperClasses.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 
 /**
@@ -12,7 +12,6 @@ public class Up implements LightPattern {
     double time;
     double speed;
     int hue;
-    double position = 0;
     int trailLength;
     boolean requestingReset = false;
 
@@ -29,27 +28,44 @@ public class Up implements LightPattern {
      * This {@link Up}'s blue component.
      */
     public Up(double speed, int length, int hue, int trailLength) {
+        pattern = new Color[length];
         this.speed = speed;
         this.length = length;
         this.hue = hue;
+        this.trailLength = trailLength;
     }
 
     private void updatePattern() {
-        position = (time * speed) % pattern.length;
-        pattern[(int)position] = Color.fromHSV(hue, 255, 255);
-        for(int j = 0; j < trailLength; i++) {
-            if((int)position-j >= 0) pattern[(int)position-j] = Color.fromHSV(hue, 255, 255 - ((trailLength/255)*j));
-            else {
-                pattern[(int)position-j] = Color.fromHSV(hue, 255, 255 - ((trailLength/255)*j));
+        int position = (int)(time * speed);
+        int increment = 255 / trailLength;
+
+        if (position >= pattern.length)
+            requestingReset = true;
+        else
+            requestingReset = false;
+
+        for (int i = 0; i < pattern.length + trailLength; i++) {
+            if (i == position && increment <= 255) {
+                pattern[i % pattern.length] = Color.fromHSV(hue, 255, increment);
+                increment += 255 / trailLength;
+                position++;
+            } else {
+                pattern[i % pattern.length] = new Color(0.0, 0.0, 0.0);
             }
         }
+
+        /*pattern[(int)position] = Color.fromHSV(hue, 255, 255);
+        for(int i = 0; i < trailLength; i++){
+            pattern[(int)position-i-1] = Color.fromHSV(hue, 255, (int)(255 - (255-(255/trailLength)*(i+1))));
+            pattern[10] = Color.fromHSV(hue, 255, 255);
+        }*/
     }
 
     @Override
     public Color[] getPattern(double time) {
         this.time = time;
         updatePattern();
-        requestingReset = position >= pattern.length;
+        //requestingReset = position >= pattern.length + trailLength;
         return pattern;
     }
 
